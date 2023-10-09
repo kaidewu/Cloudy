fea08913431363836d97c6ace46d971526c999fc
Kaide <kaidewu@protonmail.com>
2023-10-09 21:15:38 +0200

    Adding checks for more security

--- src/cloudy/api/routes/mywallet/mywallet.py
+++ src/cloudy/api/routes/mywallet/mywallet.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from sqlalchemy.orm import Session
 from database.connection import SessionLocal
 from database import models
+from api.routes.error.error_log import Error
+
+import sys
+import traceback
 
 wallet_router = APIRouter()
 
@@ -14,30 +18,69 @@ def get_db():
         db.close()
 
 @wallet_router.get("/user/{user_id}/wallet", response_description="List user wallet information")
-def get_user_wallet(
+async def get_user_wallet(
     user_id: int,
     db: Session = Depends(get_db)
     ):
-    user_wallet = (
-        db.query(
-            models.Wallet.USER_ID,
-            models.User.USER_NAME,
-            models.User.USER_SURNAMES,
-            models.User.USER_MAIL,
-            models.Account.ACCOUNT_NAME,
-            models.Account.ACCOUNT_CURRENCY_TYPES_ID,
-            models.Account.ACCOUNT_BALANCE,
-            models.AccountCurrencyType.ACCOUNT_CURRENCY_TYPES_NAME,
-            models.AccountCurrencyType.ACCOUNT_CURRENCY_TYPES_DESCRIPTION_EN
-        )
-        .join(models.User)
-        .join(models.Account)
-        .join(models.AccountCurrencyType)
-        .filter(models.Wallet.USER_ID == user_id, 
-                models.User.USER_ACTIVE == 1,
-                models.AccountCurrencyType.ACCOUNT_CURRENCY_TYPES_DELETED == 0,
-                models.Account.ACCOUNT_DELETED == 0)
-        .all()
-    )
+    results = {}
 
-    return {"data": str(user_wallet)}
+    try:
+        wallets = []
+
+        user_wallets = (
+            db.query(
+                models.Wallet.USER_ID,
+                models.User.USER_NAME,
+                models.User.USER_SURNAMES,
+                models.User.USER_MAIL,
+                models.Account.ACCOUNT_NAME,
+                models.Account.ACCOUNT_BALANCE,
+                models.AccountCurrencyType.ACCOUNT_CURRENCY_TYPES_NAME
+            )
+            .join(models.User)
+            .join(models.Account)
+            .join(models.AccountCurrencyType)
+            .filter(models.Wallet.USER_ID == user_id,
+                    models.Wallet.WALLET_DELETED == False,
+                    models.User.USER_ACTIVE == 1,
+                    models.AccountCurrencyType.ACCOUNT_CURRENCY_TYPES_DELETED == False,
+                    models.Account.ACCOUNT_DELETED == False)
+            .all()
+        )
+
+        if (user_wallets is None) or (user_wallets == []):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "statusCode": status.HTTP_400_BAD_REQUEST,
+                    "message": f"The user {user_id} wallet not exists"
+                }
+            )
+        
+        for wallet in user_wallets:
+            wallets.append({
+                "userId": wallet[0],
+                "userName": wallet[1],
+                "userSurname": wallet[2],
+                "userMail": wallet[3],
+                "accountName": wallet[4],
+                "accountBalance": wallet[5],
+                "accountCurrency": wallet[6]
+            })
+
+        results.update({
+            "status": status.HTTP_200_OK,
+            "userWallets": wallets
+        })
+
+        return results
+    except:
+        results.update(Error(
+            traceback.format_exc(),
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            f"http://192.168.1.47/api/v1/{user_id}/wallet"
+        ).insert_error_db())
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=results
+        )
