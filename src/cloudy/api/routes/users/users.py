ed57b746bc9b44d909b94ef65769302e1e752ee4
Kaide <kaidewu@protonmail.com>
2023-10-09 21:15:58 +0200

    Adding checks for security

--- src/cloudy/api/routes/users/users.py
+++ src/cloudy/api/routes/users/users.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from sqlalchemy.orm import Session
 from database.connection import SessionLocal
 from database import models
+from api.routes.error.error_log import Error
+
+import traceback
 
 users_router = APIRouter()
 
@@ -14,8 +17,40 @@ def get_db():
         db.close()
 
 @users_router.get("/users", response_description="List all users")
-def get_users(
+async def get_users(
     db: Session = Depends(get_db)
     ):
-    all_users = db.query(models.User).filter(models.User.USER_ACTIVE == True).all()
-    return all_users
+    results = {}
+
+    try:
+        all_users = db.query(models.User).filter(models.User.USER_ACTIVE == True).all()
+        if (all_users is None) or (all_users == []):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "statusCode": status.HTTP_400_BAD_REQUEST,
+                    "message": "Users are not registered"
+                }
+            )
+        
+        results.update({
+            "status": status.HTTP_200_OK,
+            "data": all_users
+        })
+
+        return results 
+
+    except:
+        results.update(Error(
+            traceback.format_exc(),
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            f"http://192.168.1.47/api/v1/users"
+        ).insert_error_db())
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "message": "Internal Error"
+            }
+        )
