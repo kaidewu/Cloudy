a2a3255ab69a5f7809798cae8cf7080f6128c88e
Kaide <kaidewu@protonmail.com>
2023-10-09 21:14:38 +0200

    Login/Registers JWT and Hash passwords

---
+++ src/cloudy/api/routes/users/utils.py
@@ -0,0 +1,45 @@
+from passlib.context import CryptContext
+import os
+from datetime import datetime, timedelta
+from typing import Union, Any
+import jwt
+from dotenv import dotenv_values
+
+env_vars = dotenv_values(f"{os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))}/.env")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = env_vars['JWT_SECRET_KEY']   # should be kept secret
+JWT_REFRESH_SECRET_KEY = env_vars['JWT_REFRESH_SECRET_KEY'] 
+
+# Create an instance of CryptContext
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Function to hash a password
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+# Function to verify a password
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
