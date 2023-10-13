545ee7891e276905347842e65a0a705223db9ff0
Kaide <kaidewu@protonmail.com>
2023-10-13 09:59:39 +0200

    Create Loading component

---
+++ src/client/components/Loading.tsx
@@ -0,0 +1,13 @@
+const Loading = () => {
+    return (
+        <div className="bg-gray-200 w-full min-h-screen flex justify-center items-center">
+            <div className="flex min-h-screen w-full items-center justify-center bg-gray-200">
+                <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500 animate-spin">
+                    <div className="h-9 w-9 rounded-full bg-gray-200"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
