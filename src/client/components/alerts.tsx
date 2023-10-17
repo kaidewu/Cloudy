3526a9946386dffe18d04a982ad928f514fd20e0
Kaide <kaidewu@protonmail.com>
2023-10-17 18:08:44 +0200

    Some change in how it views alerts

--- src/client/components/alerts.tsx
+++ src/client/components/alerts.tsx
@@ -19,33 +19,23 @@ const Alerts = ({errorData}) => {
         <div>
             {(errorData?.status >= 500 && errorData?.status <= 509) ? (
                 /* Danger */
-                <div className="px-8 py-6 bg-red-400 text-white flex justify-between rounded">
+                <div className="px-8 py-6 bg-yellow-400 text-white flex justify-between rounded-lg w-1/3 mt-10 ml-10">
                     <div className="flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-6" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-                </svg>
-                        <p>{errorData?.errorId}</p>
-                    </div>
-                    <button className="text-red-100 hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                    </button>
+                        <span className="px-3">{errorData?.errorId}</span>
+                    </div>
                 </div>
             ): (
                 /* Warning */
-                <div className="px-8 py-6 bg-yellow-400 text-white flex justify-between rounded">
+                <div className="px-8 py-6 bg-yellow-400 text-white flex justify-between rounded-lg w-1/3 mt-10 ml-10">
                     <div className="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-6" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
-                </svg>
-                        <p>{errorData?.errorId}</p>
-                    </div>
-                    <button className="text-yellow-100 hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                         </svg>
-                    </button>
+                        <span className="px-3">{errorData?.errorId}</span>
+                    </div>
                 </div>
             )}
         </div>
